d21625d8cbbf72cb04aa8e2e2a43a1929f92958d
shevtsov dmitry <shadowstakeus28@gmail.com>
2023-09-26 18:49:30 +0300

    chapter 1, section 2 - Q12

--- src/chapter1/section2/Q11.java
+++ src/chapter1/section2/Q11.java
@@ -2,11 +2,16 @@ package chapter1.section2;
 
 import edu.princeton.cs.algs4.Date;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Q11 {
     // develop illegal date
     public static void main(String[] args) {
-        SmartDate date = new SmartDate(28, 5, 2002);
-        System.out.println(date.toString());
+        SmartDate smartDate = new SmartDate(26, 9, 2023);
+
+        System.out.println(STR. "today is \{ smartDate.toString() } and it is \{ smartDate.dayOfWeek() }" );
+
     }
 
 }
@@ -39,5 +44,21 @@ class SmartDate extends Date {
         }
 
         return STR."\{dayString}.\{monthString}.\{yearString}";
+    }
+
+    public String dayOfWeek(){
+        Map<Integer, String> daysOfWeek = new HashMap<>();
+        daysOfWeek.put(1,"Monday");
+        daysOfWeek.put(2,"Tuesday");
+        daysOfWeek.put(3,"Wednesday");
+        daysOfWeek.put(4,"Thursday");
+        daysOfWeek.put(5,"Friday");
+        daysOfWeek.put(6,"Saturday");
+        daysOfWeek.put(7,"Sunday");
+//        день_недели = (год + (месяц - 1) * 12 + день) % 7
+        int dayOfWeek = (year + (month - 1) * 12 + day) % 7;
+
+        return  daysOfWeek.get(dayOfWeek);
+
     }
 }
